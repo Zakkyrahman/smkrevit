e92e49a8a8da3685e51c5ee8b5495f499c0b8d2d
Zakky <zakkyrahman1131@gmail.com>
2024-02-20 20:25:42 +0700

    vid2

---
+++ php/lat01.php
@@ -0,0 +1,16 @@
+<?php
+echo "belajar php";
+echo '<br>';
+echo "saya siswa smk";
+echo '<h1 style=background-color:red>belajar php itu mudah</h1>
+    <p style=background-color:blue>saya suka belajar program</p>
+';
+echo 'saya belajar'.'php'.'<br>';
+echo 'tahun 2019';
+echo '<br>';
+echo 2019;
+echo '<br>';
+echo 2019 + 2;
+
+?>
+
