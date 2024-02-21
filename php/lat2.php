fe5b8b8c0b72ed86e2ec0d793856772ebdce44eb
Zakky <zakkyrahman1131@gmail.com>
2024-02-21 17:06:23 +0700

    vid3

---
+++ php/lat2.php
@@ -0,0 +1,16 @@
+<?php
+
+    $tulisan = 'saya belajar';
+
+    echo $tulisan.'<br>';
+
+    $angka = 2019;
+
+    echo 'tahun'.$angka.'<br>';
+
+    var_dump($tulisan);
+
+    echo '<br>';
+
+    var_dump((int)$angka+2);
+?>
