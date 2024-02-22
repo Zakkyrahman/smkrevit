8836292148aedf26aaabdd4c1b784fbf4a5ed9aa
Zakky <zakkyrahman1131@gmail.com>
2024-02-22 16:44:17 +0700

    vid25

---
+++ php/restoran/admin/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page|Aplikasi Restoran SMK</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-3"></div>
+            <h2>Restoran</h2>
+        </div>
+        <div class="col-md-9">
+            <div class="float-end mt-4"> logout</div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <!-- <h2>Admin Page</h2> -->
+                <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="?f=kategori&m=select">Kategori</a></li>
+                <li class="nav-item"><a class="nav-link" href="?f=menu&m=select">Menu</a></li>
+                <li class="nav-item"><a class="nav-link" href="?f=pelanggan&m=select">Pelanggan</a></li>
+                <li class="nav-item"><a class="nav-link" href="?f=order&m=select">Order</a></li>
+                <li class="nav-item"><a class="nav-link" href="?f=orderdetail&m=select">Order Detail</a></li>
+                <li class="nav-item"><a class="nav-link" href="?f=user&m=select">User</a></li>
+            </ul>
+            </div>
+            <div class="col-md-9"></div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <p class="text-center">2024-copyright@aaa.com</p>
+            </div>
+        </div>
+       
+</body>
+</html>
